2c308dd66ef12cf935be05a07c664a2be13bbc71
Abdullah khan <110154936+abdullah-khan-kakar@users.noreply.github.com>
2022-12-14 21:24:57 +0500

    Add files via upload

---
+++ calculator with user input.py
@@ -0,0 +1,15 @@
+#Starting programing
+a = input("Enter your name : ")
+print(a , "nice to meet you...")
+print(a , "now enter two numbers for calculation.")
+num_1 = input("Enter first number : ")
+num_2 = input("Enter second number : ")
+#Operators
+print("Plus is : " , int(num_1) + int(num_2))
+print("Minus is : " , int(num_1) - int(num_2))
+print("Multiply is : " , int(num_1) * int(num_2))
+print("Divide is : " , int(num_1) / int(num_2))
+print("Floor Division is : " , int(num_1) // int(num_2))
+print("Power is : " , int(num_1) ** int(num_2))
+print(a , "your calculation has been done........")
+#programming ending
